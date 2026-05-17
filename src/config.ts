770f5bad4ac34989f1c2f4a8cad43f36217af248
monsieurmusclaira <maes.victor.1@gmail.com>
2026-05-17 16:11:54 +0200

    v3.1.0 - Optimisation, SEO, Content, style
    Big changes

--- src/config.ts
+++ src/config.ts
@@ -1,2 +1,2 @@
-export const SITE_TITLE = 'Kate Voet - Director & Screenwriter';
-export const SITE_DESCRIPTION = 'The film portfolio of Kate Voet';
+export const SITE_TITLE = 'Kate Voet — Director & Screenwriter';
+export const SITE_DESCRIPTION = 'Portfolio of Kate Voet, Belgian film director and screenwriter. Award-winning VR film A Long Goodbye premiered at Venice Film Festival 2025.';
